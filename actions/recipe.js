7640a676fc12bd8c35ea2c0f1e73bf73b4d93fa4
Sean Gillespie <sean@mistersg.net>
2015-10-11 22:15:53 -0400

    Use more babel features (actions/recipe.js)

--- actions/recipe.js
+++ actions/recipe.js
@@ -1,20 +1,21 @@
-module.exports = {
-  addGrain: function (grainType, weight) {
-    return {
-      type: 'ADD_GRAIN',
-      payload: {
-        type: grainType,
-        weight: weight,
-      },
-    };
-  },
+export const ADD_GRAIN = 'ADD_GRAIN';
+export const DELETE_GRAIN = 'DELETE_GRAIN';
 
-  deleteGrain: function (id) {
-    return {
-      type: 'DELETE_GRAIN',
-      payload: {
-        id: id,
-      },
-    };
-  },
-};
+export function addGrain (grainType, weight) {
+  return {
+    type: 'ADD_GRAIN',
+    payload: {
+      type: grainType,
+      weight: weight,
+    },
+  };
+}
+
+export function deleteGrain (id) {
+  return {
+    type: 'DELETE_GRAIN',
+    payload: {
+      id: id,
+    },
+  };
+}
--- test/actions/recipe.spec.js
+++ test/actions/recipe.spec.js
@@ -1,20 +1,21 @@
-const actions = require('../../actions/recipe');
+import { ADD_GRAIN, DELETE_GRAIN, addGrain, deleteGrain } from '../../actions/recipe';
+import { should } from 'chai';
 
-require('chai').should();
+should();
 
 describe('actions', function () {
   it('should create ADD_GRAIN action', function () {
-    const action = actions.addGrain('someGrain', 10);
+    const action = addGrain('someGrain', 10);
 
-    action.type.should.equal('ADD_GRAIN');
+    action.type.should.equal(ADD_GRAIN);
     action.payload.type.should.equal('someGrain');
     action.payload.weight.should.equal(10);
   });
 
   it('should create DELETE_GRAIN action', function () {
-    const action = actions.deleteGrain(1);
+    const action = deleteGrain(1);
 
-    action.type.should.equal('DELETE_GRAIN');
+    action.type.should.equal(DELETE_GRAIN);
     action.payload.id.should.equal(1);
   });
 });

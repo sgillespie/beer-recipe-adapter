484e7fa0e3d7a7fd314f64d7f1da5bdb00eb19b5
Sean Gillespie <sean.gillespie@availity.com>
2015-10-17 07:17:15 -0400

    Add state to recipe targets component

--- components/RecipeTargetsInput.js
+++ components/RecipeTargetsInput.js
@@ -2,26 +2,47 @@ const bootstrap = require('react-bootstrap'),
       React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function () {
+    return {
+      efficiency: 0.7,
+      gravity: 1.045,
+      volume: 6.5,
+    };
+  },
+
+  onChange: function () {
+    this.setState({
+      efficiency: this.refs.efficiency.getValue(),
+      gravity: this.refs.gravity.getValue(),
+      volume: this.refs.volume.getValue(),
+    });
+  },
+
   render: function () {
     return (
         <bootstrap.Row>
           <bootstrap.Col xs={3}>
             <bootstrap.Input type="text"
                              label="Preboil Gravity (SG)"
-                             placeholder="1.045"
-                             ref="preboilGravityInput" />
+                             ref="gravity"
+                             onChange={this.onChange}
+                             defaultValue={this.state.gravity}/>
           </bootstrap.Col>
           <bootstrap.Col xs={3}>
             <bootstrap.Input type="text"
                              label="Preboil Volume (Gallons)"
                              placeholder="6.5"
-                             ref="preboilVolumeInput" />
+                             ref="volume"
+                             onChange={this.onChange}
+                             defaultValue={this.state.volume}/>
           </bootstrap.Col>
           <bootstrap.Col xs={3}>
             <bootstrap.Input type="text"
                              label="Extract Efficiency"
                              placeholder="60"
-                             ref="extractEfficiencyInput"
+                             ref="efficiency"
+                             onChange={this.onChange}
+                             defaultValue={this.state.efficiency}
                              addonAfter="%"/>
           </bootstrap.Col>
         </bootstrap.Row>
--- test/components/RecipeTargetsInput.spec.js
+++ test/components/RecipeTargetsInput.spec.js
@@ -1,7 +1,11 @@
 import jsdom from 'mocha-jsdom';
 import RecipeTargetsInput from '../../components/RecipeTargetsInput';
 import React from 'react';
-import { renderIntoDocument } from 'react-addons-test-utils';
+import {
+  findRenderedDOMComponentWithTag,
+  renderIntoDocument,
+  Simulate,
+} from 'react-addons-test-utils';
 
 require('chai').should();
 
@@ -17,14 +21,44 @@ describe('RecipeTargetsInput', function () {
   });
 
   it('contains SG input', function () {
-    recipeTargets.refs.preboilGravityInput.props.type.should.equal('text');
+    recipeTargets.refs.gravity.props.type.should.equal('text');
   });
 
   it('contains volume input', function () {
-    recipeTargets.refs.preboilVolumeInput.props.type.should.equal('text');
+    recipeTargets.refs.volume.props.type.should.equal('text');
   });
 
   it('contains efficiency input', function () {
-    recipeTargets.refs.extractEfficiencyInput.props.type.should.equal('text');
+    recipeTargets.refs.efficiency.props.type.should.equal('text');
+  });
+
+  it('should update state when efficiency is changed', function () {
+    const { gravity } = recipeTargets.refs,
+          inputNode = findRenderedDOMComponentWithTag(gravity, 'input');
+
+    inputNode.value = '1.033';
+    Simulate.change(inputNode);
+
+    recipeTargets.state.gravity.should.equal('1.033');
+  });
+
+  it('should update state when volume is changed', function () {
+    const { volume } = recipeTargets.refs,
+          input = findRenderedDOMComponentWithTag(volume, 'input');
+
+    input.value = '3';
+    Simulate.change(input);
+
+    recipeTargets.state.volume.should.equal('3');
+  });
+
+  it('should update state when efficiency is changed', function () {
+    const { efficiency } = recipeTargets.refs,
+          input = findRenderedDOMComponentWithTag(efficiency, 'input');
+
+    input.value = '1';
+    Simulate.change(input);
+
+    recipeTargets.state.efficiency.should.equal('1');
   });
 });
